b7db370138ed7c48ad2e7d3229757b16f412678e
Dauda Azeez <dauda@thankucash.com>
2025-10-26 14:06:02 +0100

    Update on business rule implementation

--- PensionSystem/PensionSystem.Domain/interfaces/IBenefitRepository.cs
+++ PensionSystem/PensionSystem.Domain/interfaces/IBenefitRepository.cs
@@ -1,6 +1,8 @@
+using PensionSystem.Domain.Entities;
+
 namespace PensionSystem.Domain.interfaces;
 
-public interface IBenefitRepository
+public interface IBenefitRepository: IGenericRepository<Benefit>
 {
     
 }
--- PensionSystem/PensionSystem.Domain/interfaces/ITransactionHistoryRepository.cs
+++ PensionSystem/PensionSystem.Domain/interfaces/ITransactionHistoryRepository.cs
@@ -1,6 +1,8 @@
+using PensionSystem.Domain.Entities;
+
 namespace PensionSystem.Domain.interfaces;
 
-public interface ITransactionHistoryRepository
+public interface ITransactionHistoryRepository: IGenericRepository<TransactionHistory>
 {
     
 }
