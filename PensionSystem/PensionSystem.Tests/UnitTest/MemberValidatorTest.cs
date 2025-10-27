bdd9c0c1ba5b2a6134754c9f697787915b5a13d4
Dauda Azeez <dauda@thankucash.com>
2025-10-27 09:31:06 +0100

    unit test added

---
+++ PensionSystem/PensionSystem.Tests/UnitTest/MemberValidatorTest.cs
@@ -0,0 +1,16 @@
+using PensionSystem.Application.DTOs;
+using PensionSystem.Application.Validators;
+
+namespace PensionSystem.Tests.UnitTest;
+
+public class MemberValidatorTests
+{
+    [Test]
+    public void MemberValidator_InvalidAge_ShouldFail()
+    {
+        var dto = new CreateMemberDto("First","Last", DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-17)), "test@example.com", null);
+        var v = new MemberDtoValidator();
+        var res = v.Validate(dto);
+        Assert.IsFalse(res.IsValid);
+    }
+}
